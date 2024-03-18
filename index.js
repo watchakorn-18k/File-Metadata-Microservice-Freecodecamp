d5072419fcb9c22a340473099f7fe29fa6ef1aa7
watchakorn-18k <wk18k@proton.me>
2024-03-18 05:08:54 +0000

    fix : success

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const multer = require('multer'); // Middleware for handling multipart/form-data
+require('dotenv').config();
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const upload = multer({ dest: 'uploads/' });
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // Access uploaded file details from req.file
+  const { originalname, mimetype, size } = req.file;
+  
+  // Send JSON response containing file details
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+});
 
 
 
